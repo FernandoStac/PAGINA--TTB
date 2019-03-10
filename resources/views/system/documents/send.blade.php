9a20528f03ea21f5607f388839ac7d0ccfb4b356
javelie <javelie@outlook.com>
2019-03-09 21:55:13 -0700

    test_number 3 notification

--- resources/views/system/documents/send.blade.php
+++ resources/views/system/documents/send.blade.php
@@ -1,24 +1,4 @@
-
-
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h5 class="display-3">Archivo cargado con éxito</h5>
-  </div>
-</div>
-
-<div class="div ">
-  <div class="container ">
-    <div class="row">
-      <div class="col md-3"></div>
-      <div class="col-md-6">
-
-        <a class="text-danger" href="{{url($filename)}}">click aqui para ver el archivo cargado</a>
-      </div>
-
-      <div class="col md-3"></div>
-    </div>
-  </div>
-</div>
+pruba que llegan los correos
 
 
 
